43609f0227c983188ae7c394d793ca70d34206c6
rafael rocha <rrcasaque@hotmail.com>
2022-09-13 17:39:15 -0300

    first upload

--- Java algorithms/src/Livro.java
+++ Java algorithms/src/Livro.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Livro {
 	
 	//encapsulamento, modifica o acesso aos atributos, não permitindo que o usuário obtenha o atributo direto
@@ -59,74 +58,4 @@ public class Livro {
 
 	public void setPreco(double preco) {
 		this.preco = preco;
-	}
-	
-	
-		
-=======
-public class Livro {
-	
-	//encapsulamento, modifica o acesso aos atributos, não permitindo que o usuário obtenha o atributo direto
-	//sendo necessário criar interfaces de acesso
-	
-	private String titulo;
-	private String autor;	
-	private double preco;		
-	
-	public Livro() { //construtor vazio, vai alocar os valores padrões às variáveis
-		
-	}
-
-	//Eclipse, atalho para criar construtor ctrl + 3 "gcuf"
-	
-	public Livro(String titulo, String autor, double preco) {		
-		this.titulo = titulo;
-		this.autor = autor;
-		this.preco = preco;
-	}
-
-	public Livro(String titulo, String autor) {		
-		this.titulo = titulo;
-		this.autor = autor;
-	}
-	
-	public void atualizarPreco(double ajuste){
-		this.preco = this.preco + (this.preco * ajuste);
-	} 
-	
-	public void mostrarDados() {
-		System.out.println("Titulo: "+this.titulo + 
-				"\nAutor: "+this.autor+
-				"\nPreco: "+this.preco);		
-	}
-
-	//Atalho pra criar get e set => ctrl + 3 "ggas" 
-	
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getAutor() {
-		return autor;
-	}
-
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-
-	public double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-	
-	
-		
->>>>>>> 5ac35ecb5795d416ff48bd053d49cf9834705011
-}
+	}}
